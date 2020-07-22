bc42e2a457cb134d9efde4c482b85ee744d67fdb
Jiwon Park <minerva1993@gmail.com>
2020-07-22 12:20:54 +0900

    testing

--- macro/run_ana.py
+++ macro/run_ana.py
@@ -4,6 +4,7 @@ import argparse, psutil
 
 from analysis_helper import *
 from datasets import *
+import numpy as np
 #from histos import *
 #from cuts import *
 #from systematic import *
@@ -31,7 +32,7 @@ for d_tmp in list_to_process:
   if d_tmp != "TT_powheg_ttbb": continue
 
   for s_tmp in syst1 + syst2:
-    if s_tmp != "": continue
+    if s_tmp != "jecup": continue
     list_args.append((d_tmp, s_tmp, len(get_file_list(rootDir[year], d_tmp))))
 
 print list_args
@@ -53,9 +54,55 @@ def run_ana(tuple_args):
   if syst_ext == "": postfix = ""
   else:              postfix = "__"
 
-  d = uproot.lazyarrays([os.path.join(rootDir[year], tuple_args[0]) + '/*'], 'fcncLepJets/tree')
-  pt_sel = [(d['jet_pt'] > 30).sum() >= 3]
-  print pt_sel
+  #JER/C treatment
+  jerc_str = ''; met_idx = '';
+  if syst_ext == 'jecup':
+    jerc_str = 'jet_JES_Up'
+    met_idx  = 0
+  elif syst_ext == 'jecdown':
+    jerc_str = 'jet_JES_Down'
+    met_idx  = 1
+  elif syst_ext == 'jerup':
+    jerc_str = 'jet_JER_Up'
+    met_idx  = 2
+  elif syst_ext == 'jerdown':
+    jerc_str = 'jet_JER_Down'
+    met_idx  = 3
+  elif not isData:
+    jerc_str = 'jet_JER_Nom'
+
+  #d = uproot.lazyarrays([os.path.join(rootDir[year], tuple_args[0]) + '/*'], 'fcncLepJets/tree')
+  d = uproot.lazyarrays([os.path.join(rootDir[year], tuple_args[0]) + '/*000*'], 'fcncLepJets/tree')
+
+  #One should define additional column beforehand
+  tmp_pv = np.empty(len(d)); tmp_pv.fill(wrongPVRate(folder_to_process, year))
+  tmp_wg = np.empty(len(d)); tmp_wg.fill(1.0)
+  d['wrongPVrate']  = tmp_pv
+  d['EventWeight']  = tmp_wg
+
+  if 'jec' in syst_ext:
+    d['jet_pt_jerc'] = d['jet_pt'] * (d['jet_JER_Nom'] * d[jerc_str])
+  else:
+    d['jet_pt_jerc'] = d['jet_pt'] * d[jerc_str]
+
+  if any(i in syst_ext for i in ['jec','jer']):
+    d['met_pt_jerc']  = np.sqrt(d['MET_unc_x'][:,met_idx]**2 + d['MET_unc_y'][:,met_idx]**2)
+    d['met_phi_jerc'] = np.arctan2(d['MET_unc_x'][:,met_idx], d['MET_unc_y'][:,met_idx])
+  else: d['met_pt_jerc'] = d['MET'] 
+
+  d = d[ np.logical_or(\
+         np.logical_and(np.logical_and(d['channel'] == 0, d['lepton_pt'] > 30), abs(d['lepton_eta']) < 2.4),\
+         np.logical_and(np.logical_and(d['channel'] == 1, d['lepton_pt'] > 30), abs(d['lepton_eta']) < 2.4)\
+         )]
+  
+
+  jet_pt = d['jet_pt_jerc']
+  jet_eta = abs(d['jet_eta'])
+  jet_deepCSV = d['jet_deepCSV']
+  d = d[ (jet_eta[ jet_pt > 30 ] < 2.4).sum() >= 3 ]
+  print len(d)
+  #print d['jet_pt']
+  #print d['jet_eta']
 
 if __name__ == '__main__':
   pool = multiprocessing.Pool(1)
