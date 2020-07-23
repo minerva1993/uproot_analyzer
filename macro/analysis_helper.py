e95637eebf81e2b29825320b96bf8806b75c2546
Jiwon Park <minerva1993@gmail.com>
2020-07-23 15:33:40 +0900

    cut steps

--- macro/analysis_helper.py
+++ macro/analysis_helper.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, collections
 
 def get_file_list(rootDir, dataset):
 
@@ -9,7 +9,45 @@ def get_file_list(rootDir, dataset):
     out.append(os.path.join(rootDir,dataset,i))
 
   return out
-    
+
+
+def cut_flow():
+
+  d = collections.OrderedDict()
+  d['1'] = ' && njet_jerc == 3'
+  d['2'] = ' && njet_jerc == 3 && nbjet_jerc == 2'
+  d['3'] = ' && njet_jerc == 3 && nbjet_jerc == 3'
+  d['4'] = ' && njet_jerc >= 3 && nbjet_jerc >= 2'
+  d['5'] = ' && njet_jerc >= 4'
+  d['6'] = ' && njet_jerc >= 4 && nbjet_jerc == 2'
+  d['7'] = ' && njet_jerc >= 4 && nbjet_jerc == 3'
+  d['8'] = ' && njet_jerc >= 4 && nbjet_jerc == 4'
+
+  return d
+
+
+def b_tagging(year):
+
+  if year == 2017:
+    bWP_M = 0.4941
+    bWP_T = 0.8001
+    cvsbWP_M = 0.28
+    cvslWP_M = 0.15
+
+  elif year == 2018:
+    bWP_M = 0.4184
+    bWP_T = 0.7527
+    cvsbWP_M = 0.29
+    cvslWP_M = 0.137
+
+  else:
+    bWP_M = 0.0
+    bWP_T = 0.0
+    cvsbWP_M = 0.0
+    cvslWP_M = 0.0
+
+  return str(bWP_M)    
+
 
 def wrongPVRate(fileName, year):
 
--- macro/run_ana.py
+++ macro/run_ana.py
@@ -1,4 +1,6 @@
-import uproot, numpy
+import uproot, uproot_methods
+import matplotlib.pyplot as plt
+import mplhep as hep
 import os, sys, multiprocessing
 import argparse, psutil
 
@@ -6,13 +8,13 @@ from analysis_helper import *
 from datasets import *
 import numpy as np
 #from histos import *
-#from cuts import *
 #from systematic import *
 
 parser = argparse.ArgumentParser(description='Options for analyzer')
 parser.add_argument('--year', '-Y', action='store', type=int, default=2017, help='Set year')
 options = parser.parse_args()
 
+plt.style.use(hep.style.CMS)
 process = psutil.Process(os.getpid())
 
 year = options.year
@@ -20,7 +22,6 @@ rootDir = {2017:'/data/users/minerva1993/ntuple/V9_6/200101/production',
            2018:'/data/users/minerva1993/ntuple/V10_3/200101/production'}
 outDir = '../output/' + str(year)
 
-default_branch = ['channel','lepton_pt']
 list_to_process = dataset(year)
 
 syst1 = ['','jecup','jecdown','jerup','jerdown']
@@ -35,9 +36,8 @@ for d_tmp in list_to_process:
     if s_tmp != "jecup": continue
     list_args.append((d_tmp, s_tmp, len(get_file_list(rootDir[year], d_tmp))))
 
-print list_args
-
 def run_ana(tuple_args):
+  print 'processing... ' + str(tuple_args)
   folder_to_process = tuple_args[0]
   folder_to_process = folder_to_process.replace('_','')
   syst_ext = tuple_args[1]
@@ -85,31 +85,48 @@ def run_ana(tuple_args):
   else:
     d['jet_pt_jerc'] = d['jet_pt'] * d[jerc_str]
 
+  jet_pt = d['jet_pt_jerc']
+  jet_eta = abs(d['jet_eta'])
+  d['njet']  = np.logical_and(d['jet_pt_jerc'] > 30, abs(d['jet_eta']) < 2.4).sum()
+  d['nbjet'] = np.logical_and(np.logical_and(d['jet_pt_jerc'] > 30, abs(d['jet_eta']) < 2.4),\
+                                             d['jet_deepCSV'] > float(b_tagging(year))).sum()
+
   if any(i in syst_ext for i in ['jec','jer']):
     d['met_pt_jerc']  = np.sqrt(d['MET_unc_x'][:,met_idx]**2 + d['MET_unc_y'][:,met_idx]**2)
     d['met_phi_jerc'] = np.arctan2(d['MET_unc_x'][:,met_idx], d['MET_unc_y'][:,met_idx])
   else: d['met_pt_jerc'] = d['MET'] 
 
-  d = d[ np.logical_or(\
-         np.logical_and(np.logical_and(d['channel'] == 0, d['lepton_pt'] > 30), abs(d['lepton_eta']) < 2.4),\
-         np.logical_and(np.logical_and(d['channel'] == 1, d['lepton_pt'] > 30), abs(d['lepton_eta']) < 2.4)\
-         )]
-  
+  lepton = uproot_methods.TLorentzVectorArray.from_ptetaphie(d['lepton_pt'], d['lepton_eta'], d['lepton_phi'], d['lepton_e'])
+  met = uproot_methods.TLorentzVectorArray.from_ptetaphim(d['met_pt_jerc'], 0, d['met_phi_jerc'], 0)
+  d['lepDphi'] = lepton.delta_phi(met)
 
-  jet_pt = d['jet_pt_jerc']
-  jet_eta = abs(d['jet_eta'])
-  jet_deepCSV = d['jet_deepCSV']
-  d = d[ (jet_eta[ jet_pt > 30 ] < 2.4).sum() >= 3 ]
-  print len(d)
-  #print d['jet_pt']
-  #print d['jet_eta']
+
+  #Define cut steps
+  d = d[np.logical_or(\
+        np.logical_and(np.logical_and(d['channel'] == 0, d['lepton_pt'] > 30), abs(d['lepton_eta']) < 2.4),\
+        np.logical_and(np.logical_and(d['channel'] == 1, d['lepton_pt'] > 30), abs(d['lepton_eta']) < 2.4))]
+
+  cuts = collections.OrderedDict()
+  cuts['0'] = d
+  cuts['1'] = d[d['njet'] == 3]
+  cuts['2'] = d[np.logical_and(d['njet'] == 3, d['nbjet'] == 2)]
+  cuts['3'] = d[np.logical_and(d['njet'] == 3, d['nbjet'] == 3)]
+  cuts['4'] = d[np.logical_and(d['njet'] >= 3, d['nbjet'] >= 2)]
+  cuts['5'] = d[d['njet'] >= 4]
+  cuts['6'] = d[np.logical_and(d['njet'] >= 4, d['nbjet'] == 2)]
+  cuts['7'] = d[np.logical_and(d['njet'] >= 4, d['nbjet'] == 3)]
+  cuts['8'] = d[np.logical_and(d['njet'] >= 4, d['nbjet'] == 4)]
+
+  for step, cut_d in cuts.items():
+    print step, len(cut_d)
+
+
 
 if __name__ == '__main__':
   pool = multiprocessing.Pool(1)
   pool.map(run_ana, list_args)
   pool.close()
   pool.join()
-  #print "test"
 
 print(process.memory_info()[0]/(1024.0*1024)) #rss only
 print(process.cpu_times())
